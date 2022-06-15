28271524fbe008c8d9e5211c598ad332e78ad497
Derin Saracoglu <64663540+derinsaracoglu@users.noreply.github.com>
2022-06-15 20:27:14 +0300

    Add files via upload

---
+++ Java101/minMaxDegerler.java
@@ -0,0 +1,38 @@
+package TemelAlgoritma;
+
+import java.util.Scanner;
+
+public class minMaxDegerler {
+
+	public static void main(String[] args) {
+
+		int adet, sayi, min = 1, max = 1;
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Kaç adet sayi gireceksiniz : ");
+		adet = input.nextInt();
+
+		for (int i = 1; i <= adet; i++) {
+			System.out.print(i + ".sayiyi gir :");
+			sayi = input.nextInt();
+
+			if (i == 1) {
+				max = sayi;
+				min = sayi;
+			}
+
+			if (sayi > max) {
+				max = sayi;
+
+			}
+
+			if (sayi < min) {
+				min = sayi;
+			}
+
+		}
+		System.out.println("En buyuk sayi:" + max);
+		System.out.println("En kucuk sayi:" + min);
+	}
+
+}
