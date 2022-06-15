18906780c5cae4add23740e043a61da6dadf9fea
Derin Saracoglu <64663540+derinsaracoglu@users.noreply.github.com>
2022-06-15 20:43:03 +0300

    Add files via upload

---
+++ Java101/mukemmelSayi.java
@@ -0,0 +1,29 @@
+package TemelAlgoritma;
+
+import java.util.Scanner;
+
+public class mukemmelSayi {
+
+	public static void main(String[] args) {
+
+		int sayi, toplam = 0;
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Bir Sayi giriniz:");
+		sayi = input.nextInt();
+
+		for (int i = 1; i < sayi; i++) {
+			if (sayi % i == 0) {
+				toplam += i;
+			}
+
+		}
+		if (sayi == toplam) {
+			System.out.println(sayi + " sayisi mükemmel sayidir.");
+		} else {
+			System.out.println(sayi + " sayisi mükemmel sayi deðildir.");
+		}
+
+	}
+
+}
