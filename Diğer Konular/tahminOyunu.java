892b0a386085d77e4f52e17f6b1b4f6fa8348c0c
Derin Saracoglu <64663540+derinsaracoglu@users.noreply.github.com>
2022-06-21 17:50:29 +0300

    Add files via upload

---
+++ Diğer Konular/tahminOyunu.java
@@ -0,0 +1,65 @@
+package TemelAlgoritma;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class tahminOyunu {
+
+	public static void main(String[] args) {
+		
+		Random rand = new Random();
+		int number = rand.nextInt(100);
+		//int number =(int) (Math.random()*100);
+		
+		Scanner input = new Scanner(System.in);
+		int right =0;
+		int selected;
+		int[]wrong = new int[5];
+		boolean isWin = false;
+		boolean isWrong = false;
+		
+		
+		while(right<5) {
+			System.out.println("Tahmininizi giriniz:");
+			selected= input.nextInt();
+			
+			if(selected < 0 || selected > 99) {
+				System.out.println("Lütfen 0-100 arasýnda deðer giriniz.");
+				if(isWrong) {
+					right++;
+					System.out.println("Çok fazla hatalý giriþ yaptýnýz. Kalan hakkýnýz: " + (5-right));
+				}
+				else {
+					isWrong = true;
+					System.out.println("Bir dahaki hatalý giriþinizde hakkýnýzdan düþülecektir.");
+					
+				}
+				continue;
+			}
+			if (selected == number) {
+                System.out.println("Tebrikler, doðru tahmin ! Tahmin ettiðini sayý : " + number);
+                isWin = true;
+                break;
+            } else {
+                System.out.println("Hatalý bir sayý girdiniz !");
+                if (selected > number) {
+                    System.out.println(selected + " sayýsý, gizli sayýdan büyüktür.");
+                } else {
+                    System.out.println(selected + " sayýsý, gizli sayýdan küçüktür.");
+                }
+
+                wrong[right++] = selected;
+                System.out.println("Kalan hakký : " + (5 - right));
+            }
+        }
+
+        if (!isWin) {
+            System.out.println("Kaybettiniz ! ");
+            if (!isWrong) {
+                System.out.println("Tahminleriniz : " + Arrays.toString(wrong));
+            }
+        }
+
+    }
+}
